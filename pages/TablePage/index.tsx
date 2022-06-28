560c1145acc2da79ac0bdc18c78a11db4fb279ca
chiikana <pngnka710.pc@gmail.com>
2022-06-28 14:35:23 +0900

    ダミーのテーブル表示

--- pages/TablePage/index.tsx
+++ pages/TablePage/index.tsx
@@ -23,7 +23,70 @@ import { Header } from "../../component/header";
 
 export const HomePage = () => {
   const router = useRouter();
-  const dummyList = [];
+  let listValue = 5;
+  let listCount = 5;
+  const dummyThList = [
+    ["id", "name", "stock", "bought price", "selling price"],
+  ];
+  const dummyTbList = [
+    ["1", "リンゴ", "100", "200", "300"],
+    ["2", "バナナ", "150", "250", "350"],
+    ["3", "キウイ", "200", "300", "400"],
+    ["4", "ナシ", "250", "350", "450"],
+    ["5", "パイナップル", "300", "300", "500"],
+  ];
+  let dummyData = [
+    {
+      id: 1,
+      name: "リンゴ",
+      stock: 100,
+      bought: 200,
+      selling: 300,
+    },
+    {
+      id: 2,
+      name: "バナナ",
+      stock: 100,
+      bought: 200,
+      selling: 300,
+    },
+    {
+      id: 3,
+      name: "キウイ",
+      stock: 100,
+      bought: 200,
+      selling: 300,
+    },
+    {
+      id: 4,
+      name: "ナシ",
+      stock: 100,
+      bought: 200,
+      selling: 300,
+    },
+    {
+      id: 5,
+      name: "パイナップル",
+      stock: 100,
+      bought: 200,
+      selling: 300,
+    },
+  ];
+
+  const tableBody = () => {
+    const output = dummyData.map((value, index) => {
+      return (
+        <Tr key={index + 1}>
+          <Td>{value.id}</Td>
+          <Td>{value.name}</Td>
+          <Td>{value.stock}</Td>
+          <Td>{value.bought}</Td>
+          <Td>{value.selling}</Td>
+        </Tr>
+      );
+    });
+    return output;
+  };
   return (
     <>
       <Header />
@@ -42,8 +105,7 @@ export const HomePage = () => {
           overflowX="auto"
           overflowY="auto"
         >
-          <Table variant="striped" colorScheme="blue" size="md">
-            {/* <TableCaption>TABLE</TableCaption> */}
+          {/* <Table variant="striped" colorScheme="blue" size="md">
             <Thead
               borderBottom="1px solid #999999"
               left={0}
@@ -67,7 +129,7 @@ export const HomePage = () => {
               <Td _before={{ content: `"¥"` }}></Td>
               <Td _before={{ content: `"¥"` }}></Td>
             </Tr> */}
-              <Tr>
+          {/* <Tr>
                 <Td>1</Td>
                 <Td>リンゴ</Td>
                 <Td _after={{ content: `"個"` }}>100</Td>
@@ -103,6 +165,32 @@ export const HomePage = () => {
                 <Th>stock</Th>
                 <Th>bought price</Th>
                 <Th>selling price</Th>
+              </Tr>
+            </Tfoot>
+          </Table> */}
+          <Table variant="striped" colorScheme="blue" size="md">
+            <Thead
+              borderBottom="1px solid #999999"
+              left={0}
+              position="sticky"
+              top={0}
+            >
+              <Tr>
+                <Th>id</Th>
+                <Th>name</Th>
+                <Th>stock</Th>
+                <Th>bought</Th>
+                <Th>seling</Th>
+              </Tr>
+            </Thead>
+            <Tbody>{tableBody()}</Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>id</Th>
+                <Th>name</Th>
+                <Th>stock</Th>
+                <Th>bought</Th>
+                <Th>seling</Th>
               </Tr>
             </Tfoot>
           </Table>
