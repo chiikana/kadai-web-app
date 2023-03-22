cc01fdf0fdb8868d1babe85250663255a9c26b78
chiikana <pngnka710.pc@gmail.com>
2023-03-22 12:21:24 +0900

    firebaseを使わなくする

--- web/src/hooks/context/AuthContext.tsx
+++ web/src/hooks/context/AuthContext.tsx
@@ -1,52 +1,52 @@
-import {
-  ReactNode,
-  createContext,
-  useState,
-  useContext,
-  useEffect,
-} from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import type { User } from "firebase/auth";
-import { useRouter } from "next/router";
-import { app } from "../../libs/utils/firebase/init";
+// import {
+//   ReactNode,
+//   createContext,
+//   useState,
+//   useContext,
+//   useEffect,
+// } from "react";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import type { User } from "firebase/auth";
+// import { useRouter } from "next/router";
+// import { app } from "../../libs/utils/firebase/init";
 
-export type UserType = User | null;
+// export type UserType = User | null;
 
-export type AuthContextProps = {
-  user: UserType;
-};
+// export type AuthContextProps = {
+//   user: UserType;
+// };
 
-export type AuthProps = {
-  children: ReactNode;
-};
+// export type AuthProps = {
+//   children: ReactNode;
+// };
 
-const AuthContext = createContext<Partial<AuthContextProps>>({});
+// const AuthContext = createContext<Partial<AuthContextProps>>({});
 
-export const useAuthContext = () => {
-  return useContext(AuthContext);
-};
+// export const useAuthContext = () => {
+//   return useContext(AuthContext);
+// };
 
-export const AuthProvider = ({ children }: AuthProps) => {
-  const router = useRouter();
-  const auth = getAuth(app);
-  const [user, setUser] = useState<UserType>(null);
-  // const isAvailableForViewing =
-  //   router.pathname === "/about" ||
-  //   router.pathname === "/login" ||
-  //   router.pathname === "/signup";
-  const value = {
-    user,
-  };
+// export const AuthProvider = ({ children }: AuthProps) => {
+//   const router = useRouter();
+//   const auth = getAuth(app);
+//   const [user, setUser] = useState<UserType>(null);
+//   // const isAvailableForViewing =
+//   //   router.pathname === "/about" ||
+//   //   router.pathname === "/login" ||
+//   //   router.pathname === "/signup";
+//   const value = {
+//     user,
+//   };
 
-  useEffect(() => {
-    const authStateChanged = onAuthStateChanged(auth, async (user) => {
-      setUser(user);
-      // !user && !isAvailableForViewing && (await router.push("/login"));
-    });
-    return () => {
-      authStateChanged();
-    };
-  });
+//   useEffect(() => {
+//     const authStateChanged = onAuthStateChanged(auth, async (user) => {
+//       setUser(user);
+//       // !user && !isAvailableForViewing && (await router.push("/login"));
+//     });
+//     return () => {
+//       authStateChanged();
+//     };
+//   });
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
+//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+// };
